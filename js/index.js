628bad0c9838478dd0ff190172d137c57f9feb0c
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-10-23 11:54:50 +0900

    【Change】表の作り方

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
-dataset = [1, 2, 3, 4, 5, 6];
+var dataset = [
+  { "name": "A", "para1": 0, "para2": 5 },
+  { "name": "B", "para1": 1, "para2": 6 },
+  { "name": "C", "para1": 2, "para2": 7 },
+  { "name": "D", "para1": 3, "para2": 8 },
+  { "name": "E", "para1": 4, "para2": 9 }
+]
 
-var div = d3.selectAll("div")
-  .data(dataset);
- 
-var divEnter = div
+// 1行目のkeyだけを取り出す
+var names = d3.keys(dataset[0]);
+
+var table = d3.select("body")
+  .append("table")
+  .attr("border", "1") // 枠線表示;
+
+table.append("thead")
+  .append("tr")
+  .selectAll("th")
+  .data(names)
   .enter()
-  .append("div");
- 
-var divUpdate = divEnter.merge(div);
- 
-divUpdate
-  .style("color", "blue")
-  .text(function(d){return "this data is " + d;});
+  .append("th")
+  .text(function(d) { return d; });
 
-dataset = ['A', 'B'];
-var div = d3.selectAll("div")
+table.append("tbody")
+  .selectAll("tr")
   .data(dataset)
-  .style("color", "red")
-  .text(function(d){return "this data is " + d;});
- 
-div.exit()
-  .style("color", "green");
+  .enter()
+  .append("tr")
+  .selectAll("td")
+  // オブジェクトのキーを配列として取り出してくる
+  .data(function(row) { return d3.entries(row); })
+  .enter()
+  .append("td")
+  .text(function(d) { return d.value; });
