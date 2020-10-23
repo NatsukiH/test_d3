5191fd6bcf88ca9b3631e5bbcae53ecfd88477bb
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-10-23 11:39:58 +0900

    【Change】dataの使い方(enter, update, exit)

--- js/index.js
+++ js/index.js
@@ -1,50 +1,23 @@
-var dataset = [
-    [5, 20],
-    [480, 90],
-    [250, 50],
-    [100, 33],
-    [330, 95],
-    [410, 12],
-    [475, 44],
-    [25, 67],
-    [85, 21],
-    [220, 88]
-  ];
-  var width = 400;
-  var height = 300;
+dataset = [1, 2, 3, 4, 5, 6];
+
+var div = d3.selectAll("div")
+  .data(dataset);
  
-  var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
-  var padding = 30;
+var divEnter = div
+  .enter()
+  .append("div");
  
-  var xScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d){return d[0];})])
-    .range([padding, width - padding]);
+var divUpdate = divEnter.merge(div);
  
-  var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d){return d[1];})])
-    .range([height - padding, padding]);
+divUpdate
+  .style("color", "blue")
+  .text(function(d){return "this data is " + d;});
+
+dataset = ['A', 'B'];
+var div = d3.selectAll("div")
+  .data(dataset)
+  .style("color", "red")
+  .text(function(d){return "this data is " + d;});
  
-  var axisx = d3.axisBottom(xScale);
-  var axisy = d3.axisLeft(yScale);
-  svg.append("g")
-    .attr("transform", "translate(" + 0 + "," + (height - padding) + ")")
-    .call(axisx);
- 
-  svg.append("g")
-    .attr("transform", "translate(" + padding + "," + 0 + ")")
-    .call(axisy);
- 
-  svg.append("g")
-    .selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append("circle")
-   .attr("cx", function(d) {
-        return xScale(d[0]);
-   })
-   .attr("cy", function(d) {
-        return yScale(d[1]);
-   })
-   .attr("fill", "SkyBlue")
-   .attr("r", 4);
- ;
+div.exit()
+  .style("color", "green");
